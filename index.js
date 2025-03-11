c751027be94490faa9233abba2a79e78535432af
Edwin Vivas <jhovannyvivas@gmail.com>
2025-03-10 22:26:58 -0500

    tamaños de fuentes

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function sacarDelCarro(primerID) {
     
     articulosSeleccionados.precioTotal -= articulosSeleccionados[primerID].total;
     delete articulosSeleccionados[primerID]
-    document.getElementById(primerID+"Imagen").classList.remove("border", "border-danger", "border-3");
+    document.getElementById(primerID+"Imagen").classList.remove("border", "border-danger", "border-2","border-md-4","border-lg-2");
     document.getElementById(primerID+"BotonAdicionar").classList.remove("d-none");
         
     document.getElementById(primerID+"BotonDisminuirOAdicionar").classList.add("d-none");
@@ -52,25 +52,29 @@ function sacarDelCarro(primerID) {
     }
 }
 
-function generadorDeHtml(primerID) {
+function generadorEtiquetasParaElCarro(primerID) {
 
     let nodo = document.createElement("div");
-    nodo.classList.add("row", "border-bottom", "pb-3", "pt-3");
+    nodo.classList.add("row", "border-bottom", "pb-2", "pt-2","py-lg-2");
     nodo.setAttribute("id", primerID + "etiquetaDelArticuloEnElModal" );
 
     const plantilla = `
         <div class="col-9 p-0">
-            <p class="mb-2 fw-bold">${articulos[primerID].nombre}</p>
-            <p class="m-0"><span class="text-danger fw-bolder" id="cantidadArticuloEnElCarrio${primerID}">${articulosSeleccionados[primerID].cantidad}</span><span class="text-danger fw-bolder">x</span> &nbsp; &nbsp;
-            <span class="text-Rose-400">@ $</span><span class="text-Rose-400">${articulos[primerID].precio.toFixed(2)}</span>
+            <div class="d-flex justify-content-evenly flex-column h-100">
+            <p class="m-0 fs-5 fw-bold fs-md-4 fs-lg-8">${articulos[primerID].nombre}</p>
+            <p class="m-0 fs-5 fs-md-4 fs-lg-8"><span class="text-danger fw-bolder" id="cantidadArticuloEnElCarrio${primerID}">${articulosSeleccionados[primerID].cantidad}</span><span class="text-danger fw-bolder">x</span> &nbsp; &nbsp;
+                <span class="text-Rose-400">@ $</span><span class="text-Rose-400">${articulos[primerID].precio.toFixed(2)}</span>
 
-            &nbsp;&nbsp;
-            <span class="text-Rose-500 fw-bolder">$</span>
-            <span class="text-Rose-500 fw-bolder" id="${primerID}etiquetaPrecioXArticuloEnElCarro" >${(articulos[primerID].precio * articulosSeleccionados[primerID].cantidad).toFixed(2)}</span></p>
+                &nbsp;&nbsp;
+                <span class="text-Rose-500 fw-bolder">$</span>
+                <span class="text-Rose-500 fw-bolder" id="${primerID}etiquetaPrecioXArticuloEnElCarro" >${(articulos[primerID].precio * articulosSeleccionados[primerID].cantidad).toFixed(2)}
+                </span>
+            </p>
             </div>
-            <div class="col-3 align-self-center p-0" >
-            <div class="d-flex justify-content-end ps-3 py-3" onclick="sacarDelCarro('${primerID}')">
-            <img src="/aseets/icon-remove-item.svg" class="p-1 border  border-secondary border-2 rounded-circle" alt="">
+        </div>
+        <div class="col-3 align-self-center p-0" >
+            <div class="d-flex justify-content-end ps-3 py-3 ps-lg-1 py-lg-1 " onclick="sacarDelCarro('${primerID}')">
+                <img src="/aseets/icon-remove-item.svg" class="p-1 p-md-2 p-lg-1 border  border-secondary border-2 rounded-circle img-fluid scala65" alt="">
             </div>
         </div>
     `;
@@ -95,11 +99,12 @@ function ElementoDelArticuloEnElModal(primerID) {
                                     </div>
                                 </div>
                                 <div class="col-8 m-0 px-3 ">
-                                    <div class="d-flex flex-column justify-content-between">
-                                        <p class="mb-2 fw-bold text-truncate">${articulos[primerID].nombre}</p>
+                                    <div class="d-flex flex-column justify-content-center h-100">
+                                        <p class="mb-1 fs-6 fw-semibold   text-truncate">${articulos[primerID].nombre}</p>
                                         <div>
-                                            <p class="m-0"><span class="text-danger fw-bolder" id="cantidadDelArticuloEnElModal.${primerID}">${articulosSeleccionados[primerID].cantidad}</span><span class="text-danger fw-bolder">x</span> &nbsp; &nbsp;
-                                                <span class="text-Rose-400">@ $${articulos[primerID].precio.toFixed(2)}</span>
+                                            <p class="m-0"><span class="text-danger fs-6 fw-semibold" id="cantidadDelArticuloEnElModal.${primerID}">${articulosSeleccionados[primerID].cantidad}</span><span class="text-danger fs-6 fw-semibold">x</span> &nbsp; &nbsp; &nbsp;&nbsp;
+                                                <span class="text-Rose-400 fs-6 ">@ $${articulos[primerID].precio.toFixed(2)}</span>
+                                                </p>
                                         </div>
 
 
@@ -107,7 +112,7 @@ function ElementoDelArticuloEnElModal(primerID) {
                                 </div>
                                 <div class="col-2 m-0 p-0 ">
                                     <div class="d-flex h-100 align-items-center justify-content-center">
-                                        <p class="m-0" id="totalPrecioArticuloModal${primerID}">$${(articulos[primerID].precio * articulosSeleccionados[primerID].cantidad).toFixed(2)}</p>
+                                        <p class="m-0 fs-4 fw-semibold" id="totalPrecioArticuloModal${primerID}">$${(articulos[primerID].precio * articulosSeleccionados[primerID].cantidad).toFixed(2)}</p>
                                     </div>
                                 </div>
                         
@@ -130,7 +135,7 @@ function botonesAdicionar(primerID) {
 
     botonAdicionar.classList.add("d-none");
     botonDisminuirOAumentar.classList.remove("d-none");
-    imagen.classList.add("border", "border-danger", "border-3");
+    imagen.classList.add("border", "border-danger", "border-2","border-md-4","border-lg-2");
     agregarProductosAlCarro(primerID);
     etiquetaCantidadProducto.textContent = articulosSeleccionados[primerID]["cantidad"];
 
@@ -162,13 +167,13 @@ function agregarProductosAlCarro(primerID) {
 
         let htmlDelArticuloEnElCarro = document.getElementById(primerID +"etiquetaDelArticuloEnElModal");
         let parentNode = htmlDelArticuloEnElCarro.parentNode;
-        parentNode.replaceChild(generadorDeHtml(primerID), htmlDelArticuloEnElCarro);
+        parentNode.replaceChild(generadorEtiquetasParaElCarro(primerID), htmlDelArticuloEnElCarro);
 
 
     } else {
 
         etiquetaPadrePrecioDeLaCompra.insertBefore
-            (generadorDeHtml(primerID), etiquetaPrecioDeLaCompra);
+            (generadorEtiquetasParaElCarro(primerID), etiquetaPrecioDeLaCompra);
 
 
 
@@ -232,7 +237,7 @@ function decrementarProductos(primerID) {
         if (articulosSeleccionados[primerID]["cantidad"] == 0) {
 
             delete articulosSeleccionados[primerID];
-            document.getElementById(primerID+"Imagen").classList.remove("border", "border-danger", "border-3");
+            document.getElementById(primerID+"Imagen").classList.remove("border", "border-danger", "border-2","border-md-4","border-lg-2");
             document.getElementById(primerID+"BotonAdicionar").classList.remove("d-none");
             document.getElementById(primerID+"BotonDisminuirOAdicionar").classList.add("d-none");
             etiquetaDelArticuloEnElModal.parentNode.removeChild(etiquetaDelArticuloEnElModal);
@@ -294,56 +299,64 @@ const articulos = {
     { 
     precio: 7.00, 
     nombre: "Vanilla Bean Crème Brûlée",
-    urlThumbnail: "aseets/image-creme-brulee-thumbnail.jpg"
+    urlThumbnail: "aseets/image-creme-brulee-thumbnail.jpg",
+    urlTablet: "aseets/image-creme-brulee-tablet.jpg"
     },
 
     MacaronMixOfFive: 
     { 
     precio: 8.00, 
     nombre: "Macaron Mix of Five",
-    urlThumbnail: "aseets/image-macaron-thumbnail.jpg"
+    urlThumbnail: "aseets/image-macaron-thumbnail.jpg",
+    urlTablet: "aseets/image-macaron-tablet.jpg"
     },
 
     classicTiramisu: 
     { 
     precio: 5.50, 
     nombre: "Classic Tiramisu", 
-    urlThumbnail: "aseets/image-tiramisu-thumbnail.jpg"
+    urlThumbnail: "aseets/image-tiramisu-thumbnail.jpg",
+    urlTablet: "aseets/image-tiramisu-tablet.jpg"
     },
 
     pistachioBaklava: 
     { 
     precio: 4.00, 
-    nombre: "Vanilla Bean Crème Brûlée", 
-    urlThumbnail:"aseets/image-baklava-thumbnail.jpg" 
+    nombre: "Pistachio Baklava", 
+    urlThumbnail:"aseets/image-baklava-thumbnail.jpg",
+    urlTablet: "aseets/image-baklava-tablet.jpg" 
     },
 
     LemonMeringuePie: 
     { 
     precio: 5.00, 
     nombre: "Lemon Meringue Pie", 
-    urlThumbnail:"aseets/image-meringue-thumbnail.jpg" 
+    urlThumbnail:"aseets/image-meringue-thumbnail.jpg",
+    urlTablet: "aseets/image-meringue-tablet.jpg" 
     },
 
     RedVelveCake: 
     { 
     precio: 4.50, 
     nombre: "Red Velve Cake", 
-    urlThumbnail: "aseets/image-cake-thumbnail.jpg" 
+    urlThumbnail: "aseets/image-cake-thumbnail.jpg",
+    urlTablet: "aseets/image-cake-tablet.jpg" 
     },
 
     SaltedCaramelBrownie: 
     { 
     precio: 5.50, 
     nombre: "Salted Caramel Brownie",
-    urlThumbnail: "aseets/image-brownie-thumbnail.jpg" 
+    urlThumbnail: "aseets/image-brownie-thumbnail.jpg",
+    urlTablet: "aseets/image-brownie-tablet.jpg" 
     },
     
     VanillaPannaCotta: 
     { 
     precio: 6.50, 
     nombre: "Vanilla Panna Cotta", 
-    urlThumbnail: "aseets/image-panna-cotta-thumbnail.jpg" 
+    urlThumbnail: "aseets/image-panna-cotta-thumbnail.jpg",
+    urlTablet: "aseets/image-panna-cotta-tablet.jpg" 
 },
 
 }
